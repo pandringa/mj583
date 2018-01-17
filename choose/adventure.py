c6713c559262f16aaaad8c668450cec476ef180b
Peter Andringa <peter@andrin.ga>
2018-01-16 23:05:54 -0500

    refactor and clean homework for 1/16

--- choose/adventure.py
+++ choose/adventure.py
@@ -1,33 +1,34 @@
-import json
+from json import load as json
 from glob import glob
 
-def intInput(): 
+def selectOne(options): 
   try:
-    return int(input("> "))
+    i = int(input("> "))
+    if i >= len(options):
+      print("Input should be between 0 and %i." % (len(options)-1))
+      return selectOne(options)
   except Exception as e:
     print("Input an integer corresponding with your choice.")
-    return intInput()
+    return selectOne(options)
+  return options[i]
 
+files = glob("./*.json")
+if files:
+  print("Choose your adventure:")
+  
+  for i, file in enumerate(files):
+    print("%i: %s" % (i, file))
+  
+  data = json(open( selectOne(files) ))
+  current = data['steps'][data['startStep']]
+  while(True):
+    print(current['text'])
+    if 'options' not in current: break;
+    for i, option in enumerate(current['options']):
+      print("%i: %s" % (i, option['text']))
 
-def main():
-  files = glob("./*.json")
-  if files:
-    print("Choose your adventure:")
-    
-    for i, file in enumerate(files):
-      print("%i: %s" % (i, file))
-    data = json.load(open(files[intInput()]))
-    
-    current = data['steps'][data['startStep']]
-    while(True):
-      print(current['text'])
-      if 'options' not in current: break;
-      for i, option in enumerate(current['options']):
-        print("%i: %s" % (i, option['text']))
-      answer = current['options'][intInput()]
-      current = data['steps'][answer['goto']]
+    answer = selectOne(current['options'])
+    current = data['steps'][answer['goto']]
 
-  else:
-    print("Found no json adventure files in this directory.")
-
-main();
+else:
+  print("Found no json adventure files in this directory.")
